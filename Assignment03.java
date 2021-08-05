24812f3a5fa4d0b1ce12ddde3778a0024432e55c
Mohammad Homayoun Kamalzai <homayoun.kamalzai@gmail.com>
2021-08-05 13:59:37 -0400

    thank you professor

---
+++ Assignment03.java
@@ -0,0 +1,24 @@
+// New Project Assignment03 by Mohammad Homayoun Kamalzai
+public class Assignment03 {
+
+	public static void main(String[] args) {
+
+		int roll = 0;
+		int[] diceRolls = new int[20];
+		int totalRolls = 0;
+
+		
+// sample the rolls
+		for (int count = 0; count < 1000; count++) {
+			roll = (int) (Math.random() * 19) + 1;
+			diceRolls[roll] = diceRolls[roll - 1] + 1; // crashes
+		}
+// run a report
+		for (int index = 0; index < diceRolls.length; index++) {
+			System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+			totalRolls = totalRolls + diceRolls[index];
+		}
+		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("Mohammad Homayoun Kamalzai");
+	}
+}
